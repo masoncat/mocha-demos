f12648f1cae39208995a564e2931fffeb0466886
liuweiming02 <liuweiming02@meituan.com>
2018-06-27 16:05:04 +0800

    更多的jest的断言命令

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,8 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(4);
 });
+test('test expect',()=>{
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined;
+});
